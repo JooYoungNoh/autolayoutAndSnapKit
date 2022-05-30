e5fadcedda861635b689b73d81db9d02ed777c92
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-30 22:56:31 +0900

    initial Commit

---
+++ autolayout_practice/autolayout_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autolayout_practice
+//
+//  Created by 노주영 on 2022/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
