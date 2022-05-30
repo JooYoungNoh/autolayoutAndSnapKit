b3a3f80fcfed121957b048ec6b13a8d90497377c
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-31 03:11:45 +0900

    add: UI 구성

--- snapKit_practice/snapKit_practice/ViewController.swift
+++ snapKit_practice/snapKit_practice/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     lazy var redBox = { () -> UIView in
         let view = UIView()
         view.backgroundColor = .red
-        view.translatesAutoresizingMaskIntoConstraints = false
+      // view.translatesAutoresizingMaskIntoConstraints = false
         
         return view
     }()
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
     lazy var yellowBox = { () -> UIView in
         let view = UIView()
         view.backgroundColor = .yellow
-        view.translatesAutoresizingMaskIntoConstraints = false
+      //  view.translatesAutoresizingMaskIntoConstraints = false
         
         return view
     }()
@@ -37,20 +37,35 @@ class ViewController: UIViewController {
     lazy var blueBox = { () -> UIView in
         let view = UIView()
         view.backgroundColor = .blue
-        view.translatesAutoresizingMaskIntoConstraints = false
+       // view.translatesAutoresizingMaskIntoConstraints = false
         
         return view
     }()
+    
+    lazy var myButton = { (color: UIColor) -> UIButton in
+        let btn = UIButton(type: .system)
+        btn.backgroundColor = color
+        btn.setTitle("내 버튼", for: .normal)
+        btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 40)
+        btn.setTitleColor(.white, for: .normal)
+        btn.layer.cornerRadius = 16
+        
+        return btn
+    }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        self.view.addSubview(yellowBox)
         self.view.addSubview(greenBox)
         self.view.addSubview(redBox)
-        self.view.addSubview(yellowBox)
         self.view.addSubview(blueBox)
+        
+        let myDarkGratBtn = myButton(.darkGray)
+        
+        self.view.addSubview(myDarkGratBtn)
         
         // 기존 yellowBox
        /* yellowBox.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
@@ -62,11 +77,38 @@ class ViewController: UIViewController {
         
         // snapKit yellow Box
         yellowBox.snp.makeConstraints { make in
-            make.edges.equalTo(self.view).inset(UIEdgeInsets(top: 50, left: 20, bottom: 50, right: 20))       //탑 바텀 리딩 트레일링 전부다
+            make.edges.equalTo(self.view).inset(UIEdgeInsets(top: 40, left: 20, bottom: 40, right: 20))       //탑 바텀 리딩 트레일링 전부다
             
             //make.edges.equalToSuperview().inset(UIEdgeInsets(top: 50, left: 20, bottom: 50, right: 20))       //위와 같은 말
         }
+        
+        redBox.snp.makeConstraints { make in
+            make.width.height.equalTo(100)                                      //크기
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(20)  //위 거리
+            make.centerX.equalToSuperview()                                     //x 좌표
+        }
+        
+        blueBox.snp.makeConstraints { make in
+            make.width.equalTo(redBox.snp.width).dividedBy(1.5)           //크기 나누기
+            make.height.equalTo(redBox.snp.height).multipliedBy(1.5)    //크기 키우기
+            make.top.equalTo(redBox.snp.bottom).offset(20)              //offset은 띄우는 거리
+            make.centerX.equalToSuperview()
+        }
+        
+        greenBox.snp.makeConstraints { make in
+            make.width.height.equalTo(100)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(blueBox.snp.bottom).offset(20)
+        }
+        
+        myDarkGratBtn.snp.makeConstraints { make in
+            make.width.equalTo(200)
+            make.height.equalTo(100)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).offset(-30)
+            make.centerX.equalToSuperview()
+        }
     }
+    
 
 
 }
