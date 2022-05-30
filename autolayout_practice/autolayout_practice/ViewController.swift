ccd32f728b436b5f9b794a8afd9c6a2c2fe323ee
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-31 00:17:43 +0900

    add: 뷰 cent x축,  뷰 크기 사용

--- autolayout_practice/autolayout_practice/ViewController.swift
+++ autolayout_practice/autolayout_practice/ViewController.swift
@@ -11,7 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //view 그리기
+        let firstView = UIView()
+        firstView.translatesAutoresizingMaskIntoConstraints = false
+        firstView.backgroundColor = UIColor.systemBlue
+        self.view.addSubview(firstView)
+        
+        //위치
+        firstView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        firstView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 200).isActive = true
+        
+        //크기
+        firstView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        firstView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+ 
+        
     }
 
 
