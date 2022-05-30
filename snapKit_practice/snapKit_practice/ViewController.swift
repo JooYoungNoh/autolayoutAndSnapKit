768f962dd4e20d21b4e671153ae8b11e92cb0447
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-31 02:13:37 +0900

    add: 오토레이아웃으로 UI 생성

--- snapKit_practice/snapKit_practice/ViewController.swift
+++ snapKit_practice/snapKit_practice/ViewController.swift
@@ -8,10 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var greenBox = { () -> UIView in
+        let view = UIView()
+        view.backgroundColor = .green
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    lazy var redBox = { () -> UIView in
+        let view = UIView()
+        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    lazy var yellowBox = { () -> UIView in
+        let view = UIView()
+        view.backgroundColor = .yellow
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    lazy var blueBox = { () -> UIView in
+        let view = UIView()
+        view.backgroundColor = .blue
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.addSubview(greenBox)
+        self.view.addSubview(redBox)
+        self.view.addSubview(yellowBox)
+        self.view.addSubview(blueBox)
+        
+        //yellowBox
+        yellowBox.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
+        yellowBox.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
+        
+        yellowBox.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        yellowBox.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -10).isActive = true
     }
 
 
