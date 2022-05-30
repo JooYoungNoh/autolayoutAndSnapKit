e0ff7cac3df5393036de032727f223f0a9bc4742
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-31 02:17:30 +0900

    add: snapKit으로 UI 구성
    translatesAutoresizingMaskIntoConstraints = false 안해도 됨

--- snapKit_practice/snapKit_practice/ViewController.swift
+++ snapKit_practice/snapKit_practice/ViewController.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     
     lazy var greenBox = { () -> UIView in
         let view = UIView()
         view.backgroundColor = .green
-        view.translatesAutoresizingMaskIntoConstraints = false
+      //  view.translatesAutoresizingMaskIntoConstraints = false
         
         return view
     }()
@@ -51,12 +52,18 @@ class ViewController: UIViewController {
         self.view.addSubview(yellowBox)
         self.view.addSubview(blueBox)
         
-        //yellowBox
-        yellowBox.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
+        // 기존 yellowBox
+       /* yellowBox.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
         yellowBox.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
         
         yellowBox.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
         yellowBox.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -10).isActive = true
+        */
+        
+        // snapKit yellow Box
+        yellowBox.snp.makeConstraints { make in
+            make.edges.equalTo(self.view).inset(UIEdgeInsets(top: 50, left: 20, bottom: 50, right: 20))       //탑 바텀 리딩 트레일링 전부다
+        }
     }
 
 
