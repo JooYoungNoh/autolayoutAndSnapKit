cd2748ee1b8d21a910a286b00eb141352c194e04
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-31 22:54:51 +0900

    add: 화면 UI 구성

--- snapKit_practice2/snapKit_practice2/ViewController.swift
+++ snapKit_practice2/snapKit_practice2/ViewController.swift
@@ -6,13 +6,76 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    var offset = 0
+    var blueViewConstraint: Constraint? = nil
+    
+    let mybackground: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemYellow
+        
+        return view
+    }()
+    
+    let myButton: UIButton = {
+       let button = UIButton()
+        button.setTitle("위로 올리기", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 25)
+        button.backgroundColor = .white
+        
+        button.layer.cornerRadius = 5
+        
+        return button
+    }()
+    
+    let myView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .blue
+        
+        view.layer.cornerRadius = 5
+        
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        uiCreate()
     }
+    
+    func uiCreate(){
+        //배경
+        self.view.addSubview(mybackground)
+        
+        mybackground.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        //버튼
+        self.view.addSubview(myButton)
+        
+        myButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(80)
+            make.width.equalTo(150)
+            make.height.equalTo(50)
+        }
+        
+        //올라가는 뷰
+        self.view.addSubview(myView)
+        
+        myView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(myButton.snp.bottom).offset(100)
+            make.width.equalTo(100)
+            make.height.equalTo(50)
+        }
+        
+    }
+    
 
 
 }
